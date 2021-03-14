2a96fc5bcf0686989936f2b91175ae9d73311ee5
Ashley <ashleyaggie@aol.com>
2021-03-13 21:37:43 -0600

    Updating files with new CSS and HTML, and finalized JS

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,11 +1,13 @@
 // Setting variables for HTML elements
 var currentDay = $('#currentDay');
 var saveBtn = $('.saveBtn');
+var clearBtn = $('.clearBtn');
 var item = $('.event');
 var hour = $('.hour');
 
 // Setting variable to equal the current hour
-var time = moment('12pm','h:sa').format("HH");
+var time = moment('8am','h:sa').format("HH");
+
 
 // Setting variables for beginning of day to before work hours
 var midnight = moment('00:00:00','HH:mm:ss').format('HH');
@@ -15,20 +17,73 @@ var startHour = moment('08:59:00','HH:mm:ss').format('HH');
 var endHour = moment('18:00:00','HH:mm:ss').format('HH');
 var dayEnd = moment('23:59:59','HH:mm:ss').format('HH');
 
-// Get events saved to local storage
-var scheduleStored = JSON.parse(localStorage.getItem("userEvents"));
-
-// If no events in local storage, set array to empty
-if (scheduleStored === null) {
-    scheduleStored = [];
-}
-
-// Show events from local storage on screen
-console.log(scheduleStored);
-
 // Adds current date to jumbotron
 var now = moment().format("dddd, MMMM Do");
 currentDay.text(now);
+
+// If there is something in localStorage, then get the events and show on screen. Else, do nothing
+if (localStorage.length !== 0) {
+    getEvents();
+}
+
+function getEvents() {
+    // Get events saved to local storage
+    // Show events from local storage on screen
+
+    // 9am
+    if (localStorage.getItem("9am")) {
+        var nineAm = $("<p>" + JSON.parse(localStorage["9am"]) + "</p>");
+        $("#nineEvent").append(nineAm[0].innerText);
+    }
+
+    // 10am
+    if (localStorage.getItem("10am")) {
+        var tenAm = $("<p>" + JSON.parse(localStorage["10am"]) + "</p>");
+        $("#tenEvent").append(tenAm[0].innerText);
+    }
+
+    // 11am
+    if (localStorage.getItem("11am")) {
+        var elevenAm = $("<p>" + JSON.parse(localStorage["11am"]) + "</p>");
+        $("#elevenEvent").append(elevenAm[0].innerText);
+    }
+
+    // 12pm
+    if (localStorage.getItem("12pm")) {
+        var twelvePm = $("<p>" + JSON.parse(localStorage["12pm"]) + "</p>");
+        $("#twelveEvent").append(twelvePm[0].innerText);
+    }
+
+    // 1pm
+    if (localStorage.getItem("1pm")) {
+        var onePm = $("<p>" + JSON.parse(localStorage["1pm"]) + "</p>");
+        $("#oneEvent").append(onePm[0].innerText);
+    }
+
+    // 2pm
+    if (localStorage.getItem("2pm")) {
+        var twoPm = $("<p>" + JSON.parse(localStorage["2pm"]) + "</p>");
+        $("#twoEvent").append(twoPm[0].innerText);
+    }
+
+    // 3pm
+    if (localStorage.getItem("3pm")) {
+        var threePm = $("<p>" + JSON.parse(localStorage["3pm"]) + "</p>");
+        $("#threeEvent").append(threePm[0].innerText);
+    }
+
+    // 4pm
+    if (localStorage.getItem("4pm")) {
+        var fourPm = $("<p>" + JSON.parse(localStorage["4pm"]) + "</p>");
+        $("#fourEvent").append(fourPm[0].innerText);
+    }
+
+    // 5pm
+    if (localStorage.getItem("5pm")) {
+        var fivePm = $("<p>" + JSON.parse(localStorage["5pm"]) + "</p>");
+        $("#fiveEvent").append(fivePm[0].innerText);
+    }
+}
 
 // When the Save button is clicked, the input in the textarea is saved to local storage ONLY if something is entered
 saveBtn.click(function (event) {
@@ -36,10 +91,11 @@ saveBtn.click(function (event) {
 
     var button = $(event.target);
     var userEvent = button.prev().val();
+    var eventHour = button.prev().prev().text();
 
     if (userEvent) {
-        scheduleStored.push(userEvent);
-        localStorage.setItem('userEvents',JSON.stringify(scheduleStored));
+        localStorage.setItem(eventHour,JSON.stringify(userEvent));
+
     }
 });
 
@@ -74,6 +130,12 @@ for (var i = 0; i < hour.length; i++) {
     else if (currentHr > time) {
         var current = $(hour[i]);
         current.next().addClass('future');
-    }
+    };
     
-}
+};
+
+// Added event listener to clear local storage when Clear Events button is clicked
+clearBtn.click(function () {
+    localStorage.clear();
+    location.reload();
+});
